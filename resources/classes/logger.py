b91a120d73706d053ce6e4cf24d554d18aaebb3a
Giftie <giftie61@hotmail.com>
2014-04-16 14:00:16 -0600

    make logger.py compatible with XBMC

--- resources/classes/logger.py
+++ resources/classes/logger.py
@@ -1,64 +1,48 @@
-"""
-Simple logging class
+# xbmc logging 
 
-This class provides a cleaner way for adding logging to every file
-
-
-Released under the MIT license
-Copyright (c) 2012, Jason Millward
-
-@category   misc
-@version    $Id: 1.5, 2013-10-20 20:40:30 CST $;
-@author     Jason Millward <jason@jcode.me>
-@license    http://opensource.org/licenses/MIT
-"""
-
-import logging
+import xbmc
 import sys
-
 
 class logger(object):
 
     def __init__(self, name, debug):
-        frmt = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            "%Y-%m-%d %H:%M:%S"
-        )
-
         if debug == True:
-            logLevel = logging.DEBUG
+            logLevel = xbmc.LOGDEBUG
         else:
-            logLevel = logging.INFO
+            logLevel = xbmc.LOGNOTICE
 
-        self.sh = logging.StreamHandler(sys.stdout)
-        self.sh.setLevel(logLevel)
-        self.sh.setFormatter(frmt)
-
-        self.fh = logging.FileHandler('autoripper.log')
-        self.fh.setLevel(logLevel)
-        self.fh.setFormatter(frmt)
-
-        self.log = logging.getLogger(name)
-        self.log.setLevel(logLevel)
-        self.log.addHandler(self.sh)
-        self.log.addHandler(self.fh)
-
-    def __del__(self):
-        self.log.removeHandler(self.sh)
-        self.log.removeHandler(self.fh)
-        self.log = None
-
+    def log( self, text ):
+        if type( text).__name__=='unicode':
+            text = text.encode('utf-8')
+        message = ('[%s] - %s' % ( __scriptname__ ,text.__str__() ) )
+        xbmc.log( msg=message, level=self.logLevel )
+        
     def debug(self, msg):
-        self.log.debug(msg)
-
+        previous = self.logLevel
+        self.logLevel = xbmc.LOGDEBUG
+        self.log(msg)
+        self.logLevel = previous
+        
     def info(self, msg):
-        self.log.info(msg)
+        previous = self.logLevel
+        self.logLevel = xbmc.LOGNOTICE
+        self.log(msg)
+        self.logLevel = previous
 
     def warn(self, msg):
-        self.log.warn(msg)
+        previous = self.logLevel
+        self.logLevel = xbmc.LOGNOTICE
+        self.log(msg)
+        self.logLevel = previous
 
     def error(self, msg):
-        self.log.error(msg)
+        previous = self.logLevel
+        self.logLevel = xbmc.LOGERROR
+        self.log(msg)
+        self.logLevel = previous
 
     def critical(self, msg):
-        self.log.critical(msg)
+        previous = self.logLevel
+        self.logLevel = xbmc.LOGERROR
+        self.log(msg)
+        self.logLevel = previous
